1519741f8606235fc0c8de3025ebf3ff53cd70db
Moses Fawole <mosesfawole2001@gmail.com>
2022-05-31 11:19:52 +0100

    final commit

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -18,6 +18,7 @@ function Recipe() {
 
   useEffect(() => {
     fetchDetails(params.name);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.name]);
   return (
     <DetailWrapper>
